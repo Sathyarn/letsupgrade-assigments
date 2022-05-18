3a8b5c5aca7204059be968850bff02b50653ccec
Sathyarn <104986836+Sathyarn@users.noreply.github.com>
2022-05-18 17:33:32 +0530

    Update 4loopquestion.cpp

--- Day9 Assigments/4loopquestion.cpp
+++ Day9 Assigments/4loopquestion.cpp
@@ -1,1 +1,24 @@
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,j,sp=40,n;
+    cout<<endl<<"Enter the value of n: ";
+    cin>>n;
+    for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=sp;j++)
+                cout<<" ";
+            for(j=1;j<=i;j++)
+            {
+                ((j==1)||(j==i))? cout<<"   "<<"1":cout<<"   "<<"0";
+            }  
+            cout<<endl;
+            sp=sp-2;
+        }
+
+    return 0;
+}
