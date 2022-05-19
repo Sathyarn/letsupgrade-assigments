66864fba30152da27163ebb0162a2c8f027f463b
Sathyarn <104986836+Sathyarn@users.noreply.github.com>
2022-05-19 15:50:43 +0530

    Update 6thPatten.cpp

--- Day9 Assigments/6thPatten.cpp
+++ Day9 Assigments/6thPatten.cpp
@@ -1,1 +1,26 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<endl<<"Enter the value of n: ";
+    cin>>n;
+    int i,j,sp;
+    //sp=((n/2)-3)/4;
+    sp=0;
+    for(i=n;i>=1;i--){
+        for(j=1;j<=i;j++)
+            cout<<"   "<<j;
+        for(j=1;j<=sp;j++)
+            cout<<" ";
+        if(i!=n)
+            j=i;
+        else
+            j=i-1;
+            for(;j>=1;j--)
+                cout<<"   "<<j;
+        sp=sp+4;
+        cout<<endl;
+        
+    }
+}
+    
