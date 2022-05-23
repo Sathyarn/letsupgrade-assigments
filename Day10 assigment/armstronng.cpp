c2415776972e8452a44c776c9b22e5f894ad89af
Sathyarn <104986836+Sathyarn@users.noreply.github.com>
2022-05-23 17:14:30 +0530

    Update armstronng.cpp

--- Day10 assigment/armstronng.cpp
+++ Day10 assigment/armstronng.cpp
@@ -1,1 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,n1,s,i,r;
 
+    cout<<endl<<"Enter the upper limit of range:";
+    cin>>r;
+
+    for(i=1;i<=r;i++)
+    {
+        s=0;
+        n=i;
+
+        while(n>0)
+        {
+            n1=n%10;
+            s=s+(n1*n1*n1);
+            n=n/10;
+        }
+        if(s==i)
+            cout<<"   "<<i;
+    }
+}
